716636114a65afa781fec8cbc7b189527a72f80f
inhooo00 <chldlsgh0987@naver.com>
2024-03-13 23:00:03 +0900

    [modify] test controller

--- src/main/java/com/example/worrybox/src/TestController.java
+++ src/main/java/com/example/worrybox/src/TestController.java
@@ -1,10 +1,14 @@
 package com.example.worrybox.src;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.nio.charset.StandardCharsets;
 
 @RestController
 @RequiredArgsConstructor
@@ -12,6 +16,9 @@ public class TestController {
 
     @GetMapping("/test")
     public ResponseEntity<String> orderList() {
-        return new ResponseEntity<>("테스트 중입니다.", HttpStatus.OK);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(new MediaType("text", "plain", StandardCharsets.UTF_8));
+        return new ResponseEntity<>("테스트 중입니다.", headers, HttpStatus.OK);
     }
+
 }
