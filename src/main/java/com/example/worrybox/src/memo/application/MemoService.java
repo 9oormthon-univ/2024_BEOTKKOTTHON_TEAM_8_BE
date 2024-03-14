36d9f3d3d0ce092ea821f8e552c67dbfcd2c5ea9
inhooo00 <chldlsgh0987@naver.com>
2024-03-14 21:57:32 +0900

    [feat] MemoService 추가

--- src/main/java/com/example/worrybox/src/memo/application/MemoService.java
+++ src/main/java/com/example/worrybox/src/memo/application/MemoService.java
@@ -1,2 +1,56 @@
-package com.example.worrybox.src.memo.application;public class MemoService {
+package com.example.worrybox.src.memo.application;
+
+import com.example.worrybox.src.memo.api.dto.request.MemoRequestDto;
+import com.example.worrybox.src.memo.api.dto.response.MemoResponseDto;
+import com.example.worrybox.src.memo.domain.Memo;
+import com.example.worrybox.src.memo.domain.repository.MemoRepository;
+import com.example.worrybox.src.user.domain.User;
+import com.example.worrybox.src.user.domain.repository.UserRepository;
+import com.example.worrybox.utils.config.BaseException;
+import com.example.worrybox.utils.execption.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class MemoService {
+
+    private final MemoRepository memoRepository;
+    private final UserRepository userRepository;
+
+    // 메모 작성
+    @Transactional
+    public String writeMemo(Long userId, MemoRequestDto memoRequestDto) throws BaseException {
+        User user = userRepository.findById(userId)
+                .orElseThrow(()
+                        -> new EntityNotFoundException("User", new Exception("user를 찾을 수 없습니다.")));
+
+        Memo memo = Memo.of(user,memoRequestDto);
+
+        memoRepository.save(memo);
+        return memo.getWorryText();
+    }
+
+    // 메모 조회
+    @Transactional(readOnly = true)
+    public List<MemoResponseDto> memoList(Long userId) throws  BaseException{
+        List<Memo> memos = memoRepository.findByUserId(userId);
+
+        List<MemoResponseDto> memoResponseDtos = new ArrayList<>(); // 반환할 값
+
+        for (Memo memo : memos) {
+            MemoResponseDto memoResponseDto = MemoResponseDto.builder()
+                    .worryText(memo.getWorryText())
+                    .build();
+
+            memoResponseDtos.add(memoResponseDto);
+
+        }
+        return memoResponseDtos;
+    }
 }
