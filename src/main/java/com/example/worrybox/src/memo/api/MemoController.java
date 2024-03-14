e54feeace69379a221c19e95ffb1d62884370507
inhooo00 <chldlsgh0987@naver.com>
2024-03-15 02:33:59 +0900

    [feat] 걱정 메모 삭제 기능

--- src/main/java/com/example/worrybox/src/memo/api/MemoController.java
+++ src/main/java/com/example/worrybox/src/memo/api/MemoController.java
@@ -6,6 +6,7 @@ import com.example.worrybox.src.memo.application.MemoService;
 import com.example.worrybox.utils.config.BaseException;
 import com.example.worrybox.utils.config.BaseResponse;
 import com.example.worrybox.utils.config.BaseResponseStatus;
+import com.example.worrybox.utils.entity.Status;
 import io.swagger.annotations.Api;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -27,7 +28,7 @@ public class MemoController {
     private final MemoService memoService;
 
     // 메모 작성
-    @Operation(summary = "걱정 메모 작성", description="걱정 메모를 작성합니다")
+    @Operation(summary = "걱정 메모 작성", description = "걱정 메모를 작성합니다")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "메모 작성을 성공했습니다"),
             @ApiResponse(responseCode = "400", description = "잘못된 요청입니다"),
@@ -41,25 +42,25 @@ public class MemoController {
 
             // 메모가 제대로 들어왔는지 검사
             BaseResponseStatus status = isJoinValid(memo);
-            if(status != BaseResponseStatus.SUCCESS) {
+            if (status != BaseResponseStatus.SUCCESS) {
                 return new BaseResponse<>(status);
             }
 
             // 제대로 들어왔다면 다음 진행
-            return new BaseResponse<>(memoService.writeMemo(userId,memoRequestDto));
+            return new BaseResponse<>(memoService.writeMemo(userId, memoRequestDto));
         } catch (BaseException e) {
             return new BaseResponse<>(e.getStatus());
         }
     }
 
     public BaseResponseStatus isJoinValid(String memo) {
-        if(memo.isBlank()) return BaseResponseStatus.JOIN_EMPTY_NAME;  // 메모가 비어있는지 확인
+        if (memo.isBlank()) return BaseResponseStatus.JOIN_EMPTY_NAME;  // 메모가 비어있는지 확인
 
         return BaseResponseStatus.SUCCESS;
     }
 
     // 메모 조회
-    @Operation(summary = "걱정 메모 조회", description="걱정 메모를 조회합니다")
+    @Operation(summary = "걱정 메모 조회", description = "걱정 메모를 조회합니다")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "메모 조회를 성공했습니다"),
             @ApiResponse(responseCode = "400", description = "잘못된 요청입니다"),
@@ -67,11 +68,30 @@ public class MemoController {
                     content = @Content(schema = @Schema(example = "INVALID_HEADER or INVALID_TOKEN")))
     })
     @GetMapping("/{userId}")
-    public BaseResponse<List<MemoResponseDto>> Memo(@PathVariable Long userId)  {
-        try{
+    public BaseResponse<List<MemoResponseDto>> Memo(@PathVariable Long userId) {
+        try {
             return new BaseResponse<>(memoService.memoList(userId));
-        } catch (Exception e){
+        } catch (Exception e) {
             BaseResponseStatus status = BaseResponseStatus.INVALID_USER;
+            return new BaseResponse<>(status);
+        }
+    }
+
+    // 메모 삭제
+    @Operation(summary = "걱정 메모 삭제", description = "걱정 메모를 삭제합니다")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "메모 삭제를 성공했습니다"),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청입니다"),
+            @ApiResponse(responseCode = "401", description = "헤더 없음 or 토큰 불일치",
+                    content = @Content(schema = @Schema(example = "INVALID_HEADER or INVALID_TOKEN")))
+    })
+    @PatchMapping("/{userId}")
+    public BaseResponse<Status> deleteMemo(@PathVariable Long userId){
+        try {
+            return new BaseResponse<>(memoService.deleteMemo(userId));
+        }
+        catch (Exception e){
+            BaseResponseStatus status = BaseResponseStatus.INVALID_MEMO;
             return new BaseResponse<>(status);
         }
     }
--- src/main/java/com/example/worrybox/src/memo/application/MemoService.java
+++ src/main/java/com/example/worrybox/src/memo/application/MemoService.java
@@ -7,6 +7,7 @@ import com.example.worrybox.src.memo.domain.repository.MemoRepository;
 import com.example.worrybox.src.user.domain.User;
 import com.example.worrybox.src.user.domain.repository.UserRepository;
 import com.example.worrybox.utils.config.BaseException;
+import com.example.worrybox.utils.entity.Status;
 import com.example.worrybox.utils.execption.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -30,7 +31,7 @@ public class MemoService {
                 .orElseThrow(()
                         -> new EntityNotFoundException("User", new Exception("user를 찾을 수 없습니다.")));
 
-        Memo memo = Memo.of(user,memoRequestDto);
+        Memo memo = Memo.of(user, memoRequestDto);
 
         memoRepository.save(memo);
         return memo.getWorryText();
@@ -38,7 +39,7 @@ public class MemoService {
 
     // 메모 조회
     @Transactional(readOnly = true)
-    public List<MemoResponseDto> memoList(Long userId){
+    public List<MemoResponseDto> memoList(Long userId) {
         List<Memo> memos = memoRepository.findByUserId(userId)
                 .orElseThrow(()
                         -> new EntityNotFoundException("Memo", new Exception("userId로 memo들을 찾을 수 없습니다.")));
@@ -46,15 +47,28 @@ public class MemoService {
         List<MemoResponseDto> memoResponseDtos = new ArrayList<>(); // 반환할 값
 
         for (Memo memo : memos) {
-            MemoResponseDto memoResponseDto = MemoResponseDto.builder()
-                    .createdAt(memo.getCreatedAt())
-                    .worryText(memo.getWorryText())
-                    .solution(memo.getSolution())
-                    .build();
+            if (memo.getStatus() == Status.A) {
+                MemoResponseDto memoResponseDto = MemoResponseDto.builder()
+                        .createdAt(memo.getCreatedAt())
+                        .worryText(memo.getWorryText())
+                        .solution(memo.getSolution())
+                        .id(memo.getId())
+                        .build();
 
-            memoResponseDtos.add(memoResponseDto);
-
+                memoResponseDtos.add(memoResponseDto);
+            }
         }
         return memoResponseDtos;
+    }
+
+    // 메모 수정
+    @Transactional
+    public Status deleteMemo(Long memoId) {
+        Memo memo = memoRepository.findById(memoId)
+                .orElseThrow(()
+                        -> new EntityNotFoundException("Memo", new Exception("memo를 찾을 수 없습니다.")));
+
+        memo.setStatus(Status.D);
+        return memo.getStatus();
     }
 }
