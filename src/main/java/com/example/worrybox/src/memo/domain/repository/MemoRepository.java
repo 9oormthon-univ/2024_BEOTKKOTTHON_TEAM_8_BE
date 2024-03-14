7c649d69e694c48b3c791f1b3f4b8958a5d3374f
inhooo00 <chldlsgh0987@naver.com>
2024-03-14 21:56:15 +0900

    [feat] MemoRepository 생성

--- src/main/java/com/example/worrybox/src/memo/domain/repository/MemoRepository.java
+++ src/main/java/com/example/worrybox/src/memo/domain/repository/MemoRepository.java
@@ -1,2 +1,14 @@
-package com.example.worrybox.src.memo.domain.repository;public interface MemoRepository {
+package com.example.worrybox.src.memo.domain.repository;
+
+import com.example.worrybox.src.memo.domain.Memo;
+import com.example.worrybox.src.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemoRepository extends JpaRepository<Memo,Long> {
+    Optional<Memo> findByUser(User user);
+
+    List<Memo> findByUserId(Long userId);
 }
