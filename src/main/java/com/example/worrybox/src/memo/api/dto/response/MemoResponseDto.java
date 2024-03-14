25da0cefc620d2d7d4f51ec9bef16393b0e0ac9c
inhooo00 <chldlsgh0987@naver.com>
2024-03-14 21:59:07 +0900

    [feat] MemoResponseDto 추가

--- src/main/java/com/example/worrybox/src/memo/api/dto/response/MemoResponseDto.java
+++ src/main/java/com/example/worrybox/src/memo/api/dto/response/MemoResponseDto.java
@@ -1,2 +1,12 @@
-package com.example.worrybox.src.memo.api.dto.response;public class MemoResponseDto {
+package com.example.worrybox.src.memo.api.dto.response;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class MemoResponseDto {
+    @ApiModelProperty("걱정 메모")
+    private String worryText;
 }
