0a28d28e7e3b12fad51f2330fc65b686fee7ead0
inhooo00 <chldlsgh0987@naver.com>
2024-03-14 21:58:35 +0900

    [feat] MemoRequestDto 추가

--- src/main/java/com/example/worrybox/src/memo/api/dto/request/MemoRequestDto.java
+++ src/main/java/com/example/worrybox/src/memo/api/dto/request/MemoRequestDto.java
@@ -1,10 +1,16 @@
-package com.example.worrybox.src.memo.api.dto;
+package com.example.worrybox.src.memo.api.dto.request;
 
+import com.example.worrybox.src.memo.api.dto.response.MemoResponseDto;
 import io.swagger.annotations.ApiModelProperty;
-import lombok.Getter;
+import lombok.*;
 
 @Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MemoRequestDto {
+
     @ApiModelProperty("걱정 메모")
     private String worryText;
+
 }
