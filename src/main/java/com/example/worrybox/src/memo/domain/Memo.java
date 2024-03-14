697684d461d7fdf4936b886264e6652302251301
inhooo00 <chldlsgh0987@naver.com>
2024-03-14 12:12:24 +0900

    [modify] Memo Entity

--- src/main/java/com/example/worrybox/src/memo/domain/Memo.java
+++ src/main/java/com/example/worrybox/src/memo/domain/Memo.java
@@ -1,4 +1,37 @@
 package com.example.worrybox.src.memo.domain;
 
-public class Memo {
+import com.example.worrybox.utils.entity.BaseEntity;
+import com.example.worrybox.utils.entity.Status;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor(access= AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Memo extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "meoe_id")
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private int password;
+
+    private String worry_start_time;
+
+    private String worry_end_time;
+
+    private String worry_text;
 }
