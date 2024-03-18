1dae4bf0e74ac4c25f085e73085e410606efb4bd
joeun-01 <jjy020121@gmail.com>
2024-03-18 21:59:45 +0900

    [modify] FCM 토큰 전달 API Swagger 예시 수정

--- src/main/java/com/example/worrybox/src/notice/api/NoticeController.java
+++ src/main/java/com/example/worrybox/src/notice/api/NoticeController.java
@@ -21,14 +21,14 @@ public class NoticeController {
     private final NoticeService noticeService;
 
     /* 중복 체크 API */
-    @Operation(summary = "닉네임 중복 체크", description="닉네임 중복체크를 진행합니다")
+    @Operation(summary = "FCM Token 전달", description="유저의 FCM Token을 전달합니다")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "사용 가능한 닉네임입니다"),
             @ApiResponse(responseCode = "400", description = "잘못된 요청입니다"),
             @ApiResponse(responseCode = "400", description = "헤더 없음 or 토큰 불일치",
                     content = @Content(schema = @Schema(example = "INVALID_HEADER or INVALID_TOKEN"))),
             @ApiResponse(responseCode = "400", description = "입력값이 잘못되었습니다."),
-            @ApiResponse(responseCode = "4001", description = "중복된 이름입니다")
+            @ApiResponse(responseCode = "4000", description = "존재하지 않는 유저입니다."),
     })
     @PostMapping("/{userId}/token")
     public BaseResponse<String> nameCheck(@PathVariable Long userId, @Valid @RequestBody PostTokenReq postTokenReq) {
