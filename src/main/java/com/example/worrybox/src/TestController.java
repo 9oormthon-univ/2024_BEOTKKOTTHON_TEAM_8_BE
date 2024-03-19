9b9c7a0468bcbd7afe29f69108dcbad6bb912ffc
inhooo00 <chldlsgh0987@naver.com>
2024-03-19 22:53:23 +0900

    [feat] 워드 클라우드 테스트 코드 추가 구현

--- src/main/java/com/example/worrybox/src/TestController.java
+++ src/main/java/com/example/worrybox/src/TestController.java
@@ -1,5 +1,6 @@
 package com.example.worrybox.src;
 
+import com.example.worrybox.src.cloud.application.CloudWordService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -9,16 +10,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class TestController {
+    private final CloudWordService cloudWordService;
 
     @GetMapping("/test")
     public ResponseEntity<String> orderList() {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(new MediaType("text", "plain", StandardCharsets.UTF_8));
         return new ResponseEntity<>("테스트 중입니다.", headers, HttpStatus.OK);
+    }
+    @GetMapping("/cloud")
+    public ResponseEntity<List<String>> cloud(){
+        return new ResponseEntity<>(cloudWordService.setKomoran(),HttpStatus.OK);
     }
 
 }
