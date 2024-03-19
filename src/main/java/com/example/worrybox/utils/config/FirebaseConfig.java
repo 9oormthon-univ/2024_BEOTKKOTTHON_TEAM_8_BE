215efef211a94594bfe86c96049cdd3e1efd3d77
inhooo00 <chldlsgh0987@naver.com>
2024-03-20 01:24:09 +0900

    [modify] url 설정 수정

--- src/main/java/com/example/worrybox/utils/config/FirebaseConfig.java
+++ src/main/java/com/example/worrybox/utils/config/FirebaseConfig.java
@@ -3,10 +3,10 @@ package com.example.worrybox.utils.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Configuration;
-
-import javax.annotation.PostConstruct;
-import java.io.FileInputStream;
+import org.springframework.core.io.ClassPathResource;
+import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
@@ -14,13 +14,15 @@ public class FirebaseConfig {
     @PostConstruct
     public void init(){
         try{
-            FileInputStream serviceAccount =
-                    new FileInputStream("src/main/java/com/example/worrybox/utils/config/worryboxFirebaseKey.json");
+            InputStream serviceAccount =
+                    new ClassPathResource("worryboxFirebaseKey.json").getInputStream();
             FirebaseOptions options = new FirebaseOptions.Builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                     .build();
-            FirebaseApp.initializeApp(options);
-        }catch (Exception e){
+            if (FirebaseApp.getApps().isEmpty()) { // FirebaseApp이 이미 초기화되어 있지 않은 경우에만 초기화 실행
+                FirebaseApp.initializeApp(options);
+            }
+        } catch (Exception e){
             e.printStackTrace();
         }
     }
