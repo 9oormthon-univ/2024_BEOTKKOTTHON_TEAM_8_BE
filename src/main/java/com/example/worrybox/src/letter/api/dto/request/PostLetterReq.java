a25525c896c6c16a7a1d5d5bebd8af87ae2e0a52
joeun-01 <jjy020121@gmail.com>
2024-03-19 23:50:51 +0900

    [modify] 미래의 나에게 보내는 편지 작성 API 글자 예외 처리 추가

--- src/main/java/com/example/worrybox/src/letter/api/dto/request/PostLetterReq.java
+++ src/main/java/com/example/worrybox/src/letter/api/dto/request/PostLetterReq.java
@@ -3,6 +3,7 @@ package com.example.worrybox.src.letter.api.dto.request;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
@@ -13,5 +14,6 @@ public class PostLetterReq {
     private String arrivalDate;
     @NotBlank(message = "편지 내용을 입력해야 합니다.")
     @Schema(description = "편지 내용", example = "미래의 나 힘내라")
+    @Size(max = 30, message = "편지는 최대 30글자입니다.")
     private String letter;
 }
